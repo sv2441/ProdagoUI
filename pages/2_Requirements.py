75801263a3d3db769e542191f3b133f2e1a2de0b
Sandeep Vishwakarma <62471058+sv2441@users.noreply.github.com>
2023-07-13 14:42:44 +0530

    Update 2_Requirements.py
    api change for deployment

--- pages/2_Requirements.py
+++ pages/2_Requirements.py
@@ -17,9 +17,9 @@ import base64
 
 load_dotenv()
 
-# os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
+os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
 
-os.environ["OPENAI_API_KEY"] = os.getenv('OPENAI_API_KEY')
+# os.environ["OPENAI_API_KEY"] = os.getenv('OPENAI_API_KEY')
     
 chat_llm = ChatOpenAI(temperature=0.0)
 
